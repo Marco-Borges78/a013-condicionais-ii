c248fbe76763f74a7c2647de29a7c350a292f5cf
Marco_Borges <mas.borges78@gmail.com>
2022-08-09 20:37:25 -0300

    Resolução dos exercícios propostos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,5 +1,5 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
-
+/* 
 if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
 } else if(nacionalidade === "argentina"){
@@ -11,5 +11,24 @@ if(nacionalidade === "brasileira"){
 } else if(nacionalidade === "colombiana"){
     console.log("a pessoa é da Colômbia!");
 } else{
-    console.log("nacionalidade não encontrada")
-}
+    console.log("nacionalidade não encontrada") */
+
+switch(nacionalidade){
+        case "brasileira":
+            console.log('a pessoa é do BRASIL!')
+            break;
+        case "argentina":
+            console.log('a pessoa é da ARGENTINA!')
+            break;
+        case "uruguaia":
+            console.log('a pessoa é do URUGUAI!')
+            break;
+        case "chilena":
+            console.log('a pessoa é do CHILE!')
+            break;
+        case "colombiana":
+            console.log('a pessoa é da COLÔMBIA!')
+            break;
+        default:
+            console.log('nacionalidade NÃO ENCONTRADA')
+    }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,7 @@
 //código a ser reescrito
+//Inseri os comandos prompt's
+let nome = prompt('Qual o seu nome?')
+let idade = prompt('Qual a sua idade?')
 
 if(nome === "José"){
 	console.log("Oi, Zé!");
@@ -7,7 +10,11 @@ if(nome === "José"){
 }
 
 if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
+	console.log("Pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+//Resposta usando if ternário
+nome==="José" ? console.log('MR: Oi, Zé!') : console.log('MR: Olá, ' + nome)
+
+idade>=18 ? console.log('MR: Pode tirar carteira de motorista!') : console.log('MR: Ainda não pode tirar carteira de motorista')
